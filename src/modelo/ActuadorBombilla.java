5468a7fab7f19474830fbfe35fc6d799a40edd4b
Rafael <dominguezbenitezrafael@gmail.com>
2026-05-28 02:15:30 +0200

    Commit subclases base ActuadorBombilla y ActuadorVentilador

---
+++ src/modelo/ActuadorBombilla.java
@@ -0,0 +1,29 @@
+package modelo;
+
+public class ActuadorBombilla extends Actuador{ //Subclase para bombillas (actuadores)
+
+    //Constructor
+    public ActuadorBombilla(){
+        super("bulb", "Bombilla", "off"); //Se construye el objeto a partir del constructor padre
+    }
+
+    //Implementación de la funcion ejecutarAccion de la clase padre Actuador
+    @Override
+    public void ejecutarAccion(String accion){
+        String accionNormalizada = accion.toLowerCase();
+
+        //Si la accion es "on" u "off" se cambia el estado a la acción. Si no lo es, se lanza una excepcion
+        if (accionNormalizada.equals("on") || accionNormalizada.equals("off")){
+            this.estado = accionNormalizada;
+        }
+        else{
+            throw new IllegalArgumentException("La acción '" + accion + "' es inválida");
+        }
+    }
+
+    //Implementación de la funcion getAccionesPosibles de la clase padre Actuador
+    @Override
+    public String[] getAccionesPosibles(){
+        return new String[]{"on", "off"};
+    }
+}
---
+++ src/modelo/ActuadorVentilador.java
@@ -0,0 +1,30 @@
+package modelo;
+
+public class ActuadorVentilador extends Actuador { // Subclase para ventiladores (actuadores)
+
+    // Constructor
+    public ActuadorVentilador() {
+        super("fan", "Ventilador", "off"); // Se construye el objeto a partir del constructor padre
+    }
+
+    // Implementación de la función ejecutarAccion de la clase padre Actuador
+    @Override
+    public void ejecutarAccion(String accion) {
+        String accionNormalizada = accion.toLowerCase();
+
+        //Si la accion es "on", "off", "low", "medium" o "high" se cambia el estado a la acción. Si no lo es, se lanza una excepcion
+        if (accionNormalizada.equals("on") || accionNormalizada.equals("off") ||
+                accionNormalizada.equals("low") || accionNormalizada.equals("medium") ||
+                accionNormalizada.equals("high")) {
+            this.estado = accionNormalizada;
+        } else {
+            throw new IllegalArgumentException("La acción '" + accion + "' es inválida");
+        }
+    }
+
+    // Implementación de la función getAccionesPosibles de la clase padre Actuador
+    @Override
+    public String[] getAccionesPosibles() {
+        return new String[]{"on", "off", "low", "medium", "high"};
+    }
+}
